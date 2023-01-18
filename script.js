ff16574efee135e29df6920bc4fb1923293fa725
Pranav Mathur <pranavmathur2.0@gmail.com>
2023-01-18 13:13:31 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function loadJoke() {
+  fetch(
+    "https://v2.jokeapi.dev/joke/Any?blacklistFlags=racist,sexist&type=single&idRange=0-319"
+  ) // Fetching a joke generator API
+    .then((response) => response.json()) // Storing the response JSON text
+    .then((data) => (document.getElementById("joke").innerText = data.joke))
+    .catch(
+      () => (document.getElementById("joke").innerText = "Please try again..") // Catching an error
+    );
+}
+
+loadJoke();
+
+document.getElementById("jokeBtn").addEventListener("click", loadJoke);
